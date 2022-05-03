47a788509434db33f78f051bb476a960a58c237d
rodneyowusu <rodneyowusu12@gmail.com>
2022-05-03 17:40:51 +0530

    Routing Requests and Parsing Using Streams And Buffer

--- NodeJs/app.js
+++ NodeJs/app.js
@@ -1,11 +1,41 @@
 //Creating A Server
 const http = require("http");
+const fs = require("fs");
 // http.createServer(rqListener)
 // //OR
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-  //process.exit();
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message2'/><button type='submit'>Send</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    //The data serves as a buffer which collects multiple chunks
+    req.on("data", (chunk) => {
+      body.push(chunk);
+      console.log(chunk);
+    });
+    //Fired when parsing is done
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      //   console.log(parsedBody);
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My First Page</title></head>");

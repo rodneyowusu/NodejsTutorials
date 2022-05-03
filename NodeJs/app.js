f4231e4d625e0c1aa1a8250d6774cb423fbb91c4
rodneyowusu <rodneyowusu12@gmail.com>
2022-05-03 16:45:34 +0530

    Understanding Requests and Sending Responses

---
+++ NodeJs/app.js
@@ -0,0 +1,17 @@
+//Creating A Server
+const http = require("http");
+// http.createServer(rqListener)
+// //OR
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method, req.headers);
+  //process.exit();
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello From Owusu</h1></body>");
+  res.write("</html>");
+  res.end();
+});
+
+server.listen(3000);

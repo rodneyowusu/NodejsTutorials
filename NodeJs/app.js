42b2b7087bde73881bfb14b4e7cc74c35cf0a61d
rodneyowusu <rodneyowusu12@gmail.com>
2022-05-03 16:55:04 +0530

    Understanding Requests and Sending Response

--- NodeJs/app.js
+++ NodeJs/app.js
@@ -5,7 +5,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
-  //process.exit()
+  //process.exit();
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My First Page</title></head>");

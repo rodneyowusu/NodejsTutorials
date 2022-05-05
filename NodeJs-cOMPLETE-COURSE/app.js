7be3b071416354761d94d617c5522f179185f385
rodneyowusu <rodneyowusu12@gmail.com>
2022-05-05 11:19:21 +0530

    Adding package.json using npm init and adding start script

---
+++ NodeJs-cOMPLETE-COURSE/app.js
@@ -0,0 +1,9 @@
+//Creating A Server
+const http = require("http");
+const routes = require("./routes");
+// http.createServer(rqListener)
+// //OR
+
+const server = http.createServer(routes.handler);
+
+server.listen(3000);

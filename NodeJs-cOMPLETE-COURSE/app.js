bfa29d029cab655794969d31c80ff28b875ec957
rodneyowusu <rodneyowusu12@gmail.com>
2022-05-05 16:05:04 +0530

    Adding Expressjs and middleware

--- NodeJs-cOMPLETE-COURSE/app.js
+++ NodeJs-cOMPLETE-COURSE/app.js
@@ -1,9 +1,23 @@
 //Creating A Server
 const http = require("http");
-const routes = require("./routes");
-// http.createServer(rqListener)
-//OR
 
-const server = http.createServer(routes.handler);
+const express = require("express");
 
-server.listen(3000);
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("In the Middleware");
+  next(); //Allows request to move to the next middleware.
+});
+
+app.use((req, res, next) => {
+  console.log("In the second Middleware");
+  res.send("Hello From Express");
+});
+
+//app becomes a valid request handler
+// const server = http.createServer(app);
+// server.listen(3000);
+
+//Express way for trhe create server
+app.listen(3000);

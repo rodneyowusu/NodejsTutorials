02ded2f156b347afa6b86c27a41921b970ab3d69
rodneyowusu <rodneyowusu12@gmail.com>
2022-05-05 10:20:14 +0530

    Node module System (Making Code More Readable)

--- NodeJs/app.js
+++ NodeJs/app.js
@@ -1,47 +1,9 @@
 //Creating A Server
 const http = require("http");
-const fs = require("fs");
+const routes = require("./routes");
 // http.createServer(rqListener)
 // //OR
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title></head>");
-    res.write(
-      "<body><form action='/message' method='POST'><input type='text' name='message2'/><button type='submit'>Send</button></form></body>"
-    );
-    res.write("</html>");
-    return res.end();
-  }
-
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    //The data serves as a buffer which collects multiple chunks
-    req.on("data", (chunk) => {
-      body.push(chunk);
-      console.log(chunk);
-    });
-    //Fired when parsing is done
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      //   console.log(parsedBody);
-      const message = parsedBody.split("=")[1];
-      fs.writeFileSync("message.txt", message);
-    });
-
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
-  }
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My First Page</title></head>");
-  res.write("<body><h1>Hello From Owusu</h1></body>");
-  res.write("</html>");
-  res.end();
-});
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
---
+++ NodeJs/routes.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message2'/><button type='submit'>Send</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    //The data serves as a buffer which collects multiple chunks
+    req.on("data", (chunk) => {
+      body.push(chunk);
+      console.log(chunk);
+    });
+    //Fired when parsing is done
+    // The return will avoid the listener being in paused state and visit that right after
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      //   console.log(parsedBody);
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+
+    // res.statusCode = 302;
+    // res.setHeader("Location", "/");
+    // return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello From Owusu</h1></body>");
+  res.write("</html>");
+  res.end();
+};
+
+module.exports = {
+  handler: requestHandler,
+  someText: "This is Rodney",
+};

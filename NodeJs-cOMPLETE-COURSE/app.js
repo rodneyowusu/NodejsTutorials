de32850dd8a2b46b9ce569aa37e344b0bd14c8ee
rodneyowusu <rodneyowusu12@gmail.com>
2022-05-17 12:57:05 +0530

    Express Router and Creating Error Page

--- NodeJs-cOMPLETE-COURSE/app.js
+++ NodeJs-cOMPLETE-COURSE/app.js
@@ -1,24 +1,18 @@
 const express = require("express");
+
 const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  );
-});
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
-app.post("/product", (req, res, next) => {
-  const obj = JSON.parse(JSON.stringify(req.body)); //This line will prevent [Object: null prototype].
-  console.log(obj);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from Express!</h1>");
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page Not Found</h1>");
 });
 
 app.listen(3000);
